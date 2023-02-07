e8264381f8623d9b80aad1cbf67db276a8b02d40
Rodrigo Poblete <pobletedev@gmail.com>
2023-02-07 12:54:19 -0300

    actualizando interfaz

--- models/sockets.js
+++ models/sockets.js
@@ -7,16 +7,15 @@ class Sockets {
 
     socketsEvents() {
         //Eventos que emiten y reciben los sockets
-        this.io.on('connection', ( socket ) => { 
-            console.log(socket.id);
-             socket.emit('mensaje-bienvenida', {
-                msg: 'Bienvenido al server',
-                fecha: new Date()
-            });
-        
-             socket.on('mensaje-to-server', (data) => {
+        this.io.on('connection', (socket) => {
+
+            socket.on('mensaje-to-server', (data) => {
                 this.io.emit('mensaje-from-server', data);
-             });
+            });
+
+            socket.on('user:typing', (data) => {
+                socket.broadcast.emit('user:typing', data);
+            });
         });
     }
 }
---
+++ public/chat.js
@@ -0,0 +1,62 @@
+
+const socket = io('https://socketio-chat-app-production.up.railway.app/');
+// const socket = io('http://localhost:8080');
+
+// Referencias
+const form = document.querySelector('#formulario');
+const mensajes = document.querySelector('#mensajes');
+const txtmensaje = document.querySelector('#txtMensaje');
+const txtUser = document.querySelector('#txtUserName');
+const entranceText = document.querySelector('#messageAnnouncement');
+const users = document.querySelector('#users');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newMsg = txtmensaje.value;
+    const userName = txtUser.value;
+
+    if(newMsg === "") return;
+    if (userName === "") return;
+
+    socket.emit('mensaje-to-server', { 
+        text: newMsg,
+        user: userName,
+    });
+
+    txtUser.disabled = true;
+    txtmensaje.focus();
+    txtmensaje.value = '';
+});
+
+socket.on('mensaje-from-server', (data) => {
+    entranceText.style.cssText = 'display: none';
+    mensajes.innerHTML += ` 
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+            <div class="fw-bold customtext">${data.user}</div>
+            <span class="fw-bold customtext"> > </span>${data.text}
+        </div>
+    </li>`;
+    users.innerHTML = ''; 
+});
+
+txtmensaje.addEventListener('input', () => {
+    socket.emit('user:typing', txtUser.value);
+});
+
+socket.on('user:typing', (data) => {
+    users.innerHTML = `${data === '' ? 'alguien' : data} esta escribiendo ... <span class="animate">🔔</span>`;
+});
+
+
+/*         socket.on('mensaje-bienvenida', (data) => {
+            console.log(data);
+        }); */
+
+/*         setTimeout(() => {
+            socket.emit('mensaje-servidor', {
+                msg: 'Cliente',
+                nombre: 'Rodrigo'
+            })
+        }, 2000); */
