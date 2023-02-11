60d7dcbc64b0a6a2c7d90601bc284f09049da469
Rodrigo Poblete <pobletedev@gmail.com>
2023-02-11 02:43:15 -0300

    añadiendo custom colors

--- public/chat.js
+++ public/chat.js
@@ -9,11 +9,23 @@ const txtmensaje = document.querySelector('#txtMensaje');
 const txtUser = document.querySelector('#txtUserName');
 const entranceText = document.querySelector('#messageAnnouncement');
 const users = document.querySelector('#users');
-const sphere = document.querySelector('#sphere-1');
+/* const sphere = document.querySelector('#sphere-1');
 const sphere2 = document.querySelector('#sphere-2');
 const sphere3 = document.querySelector('#sphere-3');
-const sphere4 = document.querySelector('#sphere-4');
+const sphere4 = document.querySelector('#sphere-4'); */
 
+const randomColors = [
+    '#E73C7E',
+    '#EE7752',
+    '#6EDF16',
+    '#16DFDC',
+    '#F3654E',
+    '#23A6D5',
+    '#DCE175',
+    '#D078EF',
+    '#23D5AB',
+    '#2B6DF1',
+]
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -39,7 +51,10 @@ socket.on('mensaje-from-server', (data) => {
     mensajes.innerHTML += ` 
     <div class="fw-bold customtext mt-2">${data.user}</div>
     <li class="list-group-item d-flex justify-content-between align-items-start mt-2 p-2">
-        <div class="message_container">
+        <div 
+            class="message_container" 
+            style='background-color: ${randomColors[Math.floor( randomColors.length * Math.random() )]}
+        '>
             <span class="user_message">${data.text}</span>
         </div>
     </li>
